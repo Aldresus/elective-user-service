7c78d8ecb5ba8f913b614f061d88d24485604411
Louis <louis.dumont@viacesi.fr>
2024-06-10 11:45:36 +0200

    test autobuild

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Users microservice JENKINS TEST')
+    .setTitle('Users microservice JENKINS')
     .setDescription('Users microservice')
     .setVersion('1.0')
     .build();
