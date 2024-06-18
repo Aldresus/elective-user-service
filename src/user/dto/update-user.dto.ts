b9b9396378561a30ef2ce07a2c99eabe5b4639f9
Zepyyy <quentin.stubecki@viacesi.fr>
2024-06-18 23:05:29 +0200

    moins d'adresse + de clients

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -12,15 +12,6 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
   email?: string;
 
   @ApiProperty({ required: false })
-  postal_code?: string;
-
-  @ApiProperty({ required: false })
-  address?: string;
-
-  @ApiProperty({ required: false })
-  city?: string;
-
-  @ApiProperty({ required: false })
   birthday?: string;
 
   @ApiProperty({ required: false })
